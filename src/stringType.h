24e377d66577ea4230077daa44090cdbd79dc53d
Adam Juraszek <juriad@gmail.com>
2011-09-09 19:49:28 +0200

    not working version

--- src/stringType.h
+++ src/stringType.h
@@ -17,42 +17,9 @@ public:
 	}
 	virtual ~StringType() {
 	}
-	virtual QString getDataType() {
-		qDebug() << "dataType" << "string";
-		return "QString";
-	}
-	virtual QString generateControl(QString inputString) {
-		return "return true";
-	}
-	virtual QString generateSetter(QString inputString,
-			QString outputVariable) {
-		if (getName() == "string") {
-			return outputVariable + " = " + inputString + ";";
-		} else if (getName() == "normalized string") {
-			QString setter =
-					"QString normalized = " + inputString
-							+ ".replace(QChar(9), QChar(32)).replace(QChar(10), QChar(32)).replace(QChar(13), QChar(32));";
-			return outputVariable + " = normalized;";
-		} else {
-			QString setter =
-					"QString normalized = " + inputString
-							+ ".replace(QChar(9), QChar(32)).replace(QChar(10), QChar(32)).replace(QChar(13), QChar(32));\n";
-			setter.append("QString trimmed = normalized.trimmed();\n");
-			setter.append("bool space = false;\n");
-			setter.append("for(int i = trimmed.size()-1; i>0; i--) {\n");
-			setter.append("if(trimmed.at(i)==QChar(32)) {\n");
-			setter.append("if(space) {\n");
-			setter.append("trimmed.remove(i);\n");
-			setter.append("} else {\n");
-			setter.append("space = true;\n");
-			setter.append("}\n");
-			setter.append("} else {\n");
-			setter.append("space = false;\n");
-			setter.append("}\n");
-			setter.append("}\n");
-			return outputVariable + " = trimmed;";
-		}
-	}
+	virtual QString getDataType();
+	virtual QString generateControl(QString inputString);
+	virtual QString generateSetter(QString inputString, QString outputVariable);
 };
 
 #endif /* STRINGTYPE_H_ */
